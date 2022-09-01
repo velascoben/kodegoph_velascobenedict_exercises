7839135d15363a6c5605f741a62d773b798dc8c9
benedict van velasco <velascobenedict@yahoo.com>
2022-09-01 20:55:06 +0800

    first commit

---
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    /*
+    Author: Benedict Van Velasco
+    Date Created: September 1, 2022
+    Filename: FirstKotlinProgram.kt
+     */
+    print("Benedict Van")
+    print(" Velasco")
+}
