ee8d6aeac246cfccc2fe62df7cab376c966f0ad7
benedict van velasco <velascobenedict@yahoo.com>
2022-09-03 11:56:30 +0800

    Commit Practice Activity 1 and 2

---
+++ src/main/kotlin/practice_activities/practice_activity2.kt
@@ -0,0 +1,33 @@
+package practice_activities
+
+fun main() {
+    /*
+    Create a Kotlin program that will accept grades, then calculates and display their average grade.
+
+    Sample Output
+
+    Enter Grade in Math:
+    78
+
+    Enter  Grade in English:
+    81
+
+    Enter  Grade in Filipino:
+    98
+
+    Average Grade is: 85.66
+     */
+
+    println("Enter Grade in Math: ")
+    var math : Double = readln().toDouble()
+
+    println("Enter Grade in English: ")
+    var english : Double = readln().toDouble()
+
+    println("Enter Grade in Filipino: ")
+    var filipino : Double = readln().toDouble()
+
+    var ave : Double = (math + english + filipino) / 3
+
+    println("Average Grade is: " + String.format("%.2f", ave).toDouble())
+}
