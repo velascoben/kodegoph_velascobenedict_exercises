a9ac4746dc2361f0c1547d88d16bc9e3dc02b35d
benedict van velasco <velascobenedict@yahoo.com>
2022-09-03 14:51:13 +0800

    Commit Activity 01 - A

--- src/main/kotlin/exercises/Activity01-A.kt
+++ src/main/kotlin/exercises/Activity01-A.kt
@@ -10,6 +10,62 @@ fun main() {
 
      */
 
+    // ------------------------------------------- DECLARATIONS ------------------------------- //
+
+    // Student Data
+    var studentID : Int = 0
+    var studentFirstName : String = ""
+    var studentMiddleName : String = ""
+    var studentLastName : String = ""
+    var studentClassCode : String = ""
+
+    // Final Grades
+    var studentFinalGrade : Double = 0.00
+    var studentMonth1Grade : Double = 0.00
+    var studentMonth2Grade : Double = 0.00
+    var studentMonth3Grade : Double = 0.00
+    var studentMonth4Grade : Double = 0.00
+
+    // Month1 Grades
+    var studentM1Week1Grade : Double = 0.00
+    var studentM1Week2Grade : Double = 0.00
+    var studentM1Week3Grade : Double = 0.00
+    var studentM1Week4Grade : Double = 0.00
+
+    // Month2 Grades
+    var studentM2Week1Grade : Double = 0.00
+    var studentM2Week2Grade : Double = 0.00
+    var studentM2Week3Grade : Double = 0.00
+    var studentM2Week4Grade : Double = 0.00
+
+    // Month3 Grades
+    var studentM3Week1Grade : Double = 0.00
+    var studentM3Week2Grade : Double = 0.00
+    var studentM3Week3Grade : Double = 0.00
+    var studentM3Week4Grade : Double = 0.00
+
+    // Month4 Grades
+    var studentM4Week1Grade : Double = 0.00
+    var studentM4Week2Grade : Double = 0.00
+    var studentM4Week3Grade : Double = 0.00
+    var studentM4Week4Grade : Double = 0.00
+
+    // ------------------------------------------- DECLARATIONS END ------------------------------- //
+
+    // ------------------------------------------- FORMULA ------------------------------- //
+
+    studentMonth1Grade = studentM1Week1Grade + studentM1Week2Grade + studentM1Week3Grade + studentM1Week4Grade
+    studentMonth2Grade = studentM2Week1Grade + studentM2Week2Grade + studentM2Week3Grade + studentM2Week4Grade
+    studentMonth3Grade = studentM3Week1Grade + studentM3Week2Grade + studentM3Week3Grade + studentM3Week4Grade
+    studentMonth4Grade = studentM4Week1Grade + studentM4Week2Grade + studentM4Week3Grade + studentM4Week4Grade
+
+    studentFinalGrade = studentMonth1Grade + studentMonth2Grade + studentMonth3Grade + studentMonth4Grade
+
+    // ------------------------------------------- FORMULA END ------------------------------- //
+
+
+    // Initial Understanding
+
     println("MD1P STUDENTS' PERFORMANCE UPDATE")
 
     var student1 : String = "Week1 = 95, Week2 = 90, Week3 = 92, Week 4 = No Grade Yet"
