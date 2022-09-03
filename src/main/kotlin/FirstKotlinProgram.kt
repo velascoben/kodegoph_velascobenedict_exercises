854f54d1194636c171cbf2890c72f8bd6fadc4d2
benedict van velasco <velascobenedict@yahoo.com>
2022-09-03 09:17:30 +0800

    commit sum program

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -5,10 +5,55 @@ fun main(){
     Date Created: September 1, 2022
     Filename: FirstKotlinProgram.kt
      */
+
+    /*
     print("Benedict Van")
     print(" Velasco")
     print(" Hellooooooooooooooooooo!")
     print(" Hellooooooooooooooooooo Again!")
     print(" Hellooooooooooooooooooo Again and Again!")
     print(" Hellooooooooooooooooooo Again and Again and Again!")
+
+     */
+
+    /*
+    var firstName : String = "Ben"
+    var age : Int = 32
+    var grade : Double = 75.00
+    var section : Char = 'A'
+    var isEnrolled : Boolean = true
+
+    println("My name is $firstName")
+    println("My age is $age")
+    println("My grade is $grade")
+    println("My section is $section")
+
+    if(isEnrolled) {
+        println("I am enrolled!")
+    } else {
+        println("I am not yet enrolled!")
+    }
+
+     */
+
+    /*
+    println("Enter your name: ")
+    var firstName : String = readln()
+
+    println("Enter your grade: ")
+    var grade : Int = readln().toInt()
+
+    println("Your name is $firstName")
+    println("Your grade is $grade")
+
+     */
+
+    println("Enter number1: ")
+    var number1 : Int = readln().toInt()
+
+    println("Enter number2: ")
+    var number2 : Int = readln().toInt()
+
+    var sum : Int = number1 + number2
+    println("The sum of $number1 and $number2 is $sum")
 }
