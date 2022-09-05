0cd1cfe1264205137cd59a32709fa4e9b71c6ef6
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 19:44:02 +0800

    Commit KotlinProgram8

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // while loop
+    // init
+
+    var ctr : Int = 5
+
+    // condition
+
+    while(ctr <= 5) {
+        println("Hello World! $ctr")
+        // update
+        ctr--
+    }
+}
