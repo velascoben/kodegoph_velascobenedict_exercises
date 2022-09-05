d4eb5a1ed8491e6e48db44525ac9c2314137988e
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 15:16:59 +0800

    Commit Activity 01 - G

---
+++ src/main/kotlin/exercises/Activity01-G.kt
@@ -0,0 +1,24 @@
+package exercises
+
+fun main() {
+    /*
+
+    Activity 01 - G
+
+    Create an application that will accept 2 Integers.
+    It will identify the lower value and bigger value of the two inputs.
+    Then, your application will print all the prime numbers starting from the lower number up to the largest number.
+
+     */
+
+    // ------------------------------------------- DECLARATIONS ------------------------------- //
+
+    var number1 : Int = 0
+    var number2 : Int = 0
+
+    // ------------------------------------------- DECLARATIONS END ------------------------------- //
+
+
+
+
+}
