15b8770c92d5c43985048b2444727151d4dbeeea
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 15:17:48 +0800

    Commit Activity 01 - I

---
+++ src/main/kotlin/exercises/Activity01-I.kt
@@ -0,0 +1,26 @@
+package exercises
+
+import java.util.*
+
+fun main() {
+    /*
+
+    Activity 01 - I
+
+    Using Activity 01 - C and implement a process where items are added to the grocery.
+
+     */
+
+    // ------------------------------------------- DECLARATIONS ------------------------------- //
+
+    var groceryCode : Int = 0 // ID number of the item added to the grocery *PRIMARY KEY*
+    var invCode : Int = 0 // Contains the invCode of the item that was added to the grocery *FOREIGN KEY*
+    var dateAdded : Date
+    var groceryCount : Int = 0 // Check how many items are left in the grocery
+
+    // ------------------------------------------- DECLARATIONS END ------------------------------- //
+
+
+
+
+}
