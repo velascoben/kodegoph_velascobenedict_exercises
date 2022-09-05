0de81cede2e5ac73b60662e8414e1b35fba7f159
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 15:17:16 +0800

    Commit Activity 01 - H

---
+++ src/main/kotlin/exercises/Activity01-H.kt
@@ -0,0 +1,24 @@
+package exercises
+
+fun main() {
+    /*
+
+    Activity 01 - H
+
+    Using Activity 01 - B and Implement a process where someone can borrow a book.
+
+     */
+
+    // ------------------------------------------- DECLARATIONS ------------------------------- //
+
+    var borrowerID : Int = 0
+    var borrowerName : String = ""
+    var borrowerAddress : String = ""
+    var bookBorrowed : Int = 0 // This will contain the ID of the book borrowed
+
+    // ------------------------------------------- DECLARATIONS END ------------------------------- //
+
+
+
+
+}
