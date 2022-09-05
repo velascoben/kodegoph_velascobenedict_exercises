6ac0715bded8e1e234cef6dd73bd2224ec7808df
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 15:15:30 +0800

    Commit Activity 01 - D

---
+++ src/main/kotlin/exercises/Activity01-D.kt
@@ -0,0 +1,33 @@
+package exercises
+
+fun main() {
+    /*
+
+    Activity 01 - D
+
+    Create an application that will accept monetary amounts.
+    The application will continue to accept values until a “No” is set as input for the question
+    “Do you want to add another amount?”.
+    After the input is done, the user will be asked “Divide the value by how many?”.
+    It will only accept an Integer as input.
+    The total of the first input will be divided by the second input.
+
+    Note: Error checking must be done.
+
+     */
+
+    // ------------------------------------------- DECLARATIONS ------------------------------- //
+
+    var amount : Double = 0.00
+    var divideBy : Int = 0
+    var again : String = ""
+    var total : Double = 0.00
+
+    // ------------------------------------------- DECLARATIONS END ------------------------------- //
+
+    println("Enter amount:")
+    amount = readln().toDouble()
+
+
+
+}
