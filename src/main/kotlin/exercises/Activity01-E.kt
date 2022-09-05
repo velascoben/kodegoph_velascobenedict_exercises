0d12e7a2c3ec59c5117b26ac9b421f9abfbd366d
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 15:16:13 +0800

    Commit Activity 01 - E

---
+++ src/main/kotlin/exercises/Activity01-E.kt
@@ -0,0 +1,23 @@
+package exercises
+
+fun main() {
+    /*
+
+    Activity 01 - E
+
+    Create an application that will accept 2 string inputs.
+    Your application will print all unique characters in both Strings.
+
+     */
+
+    // ------------------------------------------- DECLARATIONS ------------------------------- //
+
+    var string1 : String = ""
+    var string2 : String = ""
+
+    // ------------------------------------------- DECLARATIONS END ------------------------------- //
+
+
+
+
+}
