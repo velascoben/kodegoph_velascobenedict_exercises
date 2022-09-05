4f8bea986b74d963a3da5eab1b272841d90a809e
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 15:16:34 +0800

    Commit Activity 01 - F

---
+++ src/main/kotlin/exercises/Activity01-F.kt
@@ -0,0 +1,22 @@
+package exercises
+
+fun main() {
+    /*
+
+    Activity 01 - F
+
+    Create an application that will accept 1 string.
+    Your application will print “Palindrome” if the string is a palindrome.
+
+     */
+
+    // ------------------------------------------- DECLARATIONS ------------------------------- //
+
+    var string1 : String = ""
+
+    // ------------------------------------------- DECLARATIONS END ------------------------------- //
+
+
+
+
+}
