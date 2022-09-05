dbe5786bf5a27750ea6867c5d518530bd49cbff3
benedict van velasco <velascobenedict@yahoo.com>
2022-09-05 20:45:24 +0800

    Commit Changes in KotlinProgram10

--- src/main/kotlin/KotlinProgram10.kt
+++ src/main/kotlin/KotlinProgram10.kt
@@ -3,11 +3,13 @@ fun main() {
     while(ctr1 <= 5) {
         var ctr2 : Int = 1
         while (ctr2 <=5) {
-            //print("@")
-            print(ctr1 * ctr2)
+            print("@")
+            //print(ctr1 * ctr2)
             ctr2++
+            // This is where @@@@@ happens
         }
         println()
         ctr1++
+        // This is where next line happens
     }
 }
