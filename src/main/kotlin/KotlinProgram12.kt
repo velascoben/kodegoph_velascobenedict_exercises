08c541cc9e4877c1a6a5af7d08e3792e0fe3c8f5
benedict van velasco <velascobenedict@yahoo.com>
2022-09-06 19:34:27 +0800

    Commit KotlinProgram12

---
+++ src/main/kotlin/KotlinProgram12.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var myString : String? = "Hello"
+
+    //println(myString)
+
+    // ?.let {}
+//    var count = myString?.let { 2+2 }
+//    println(count)
+
+    // Elvis Operator ?:
+//    val count2 = myString?.length ?: -1
+//    println(count2)
+
+    // !!
+//    val count3 = myString!!.length
+//    println(count3)
+
+}
