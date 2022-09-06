b2a19facb203a90344c0e391ced193e8e2ed5ded
benedict van velasco <velascobenedict@yahoo.com>
2022-09-06 18:49:58 +0800

    Commit KotlinProgram9

---
+++ src/main/kotlin/KotlinProgram9.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // while loop
+    // init
+
+    var ctr: Int = 1
+
+    do {
+
+        println("Hello World!")
+        // update
+        ctr++
+
+    }while (ctr <= 5) // condition
+}
