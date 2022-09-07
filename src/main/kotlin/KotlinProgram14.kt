81484b7fa5212228868ebd150e7e8c07be7af1a3
benedict van velasco <velascobenedict@yahoo.com>
2022-09-07 20:23:05 +0800

    Commit KotlinProgram14

---
+++ src/main/kotlin/KotlinProgram14.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var names = arrayOf<String>("John", "Paul", "Mark", "Ringo", "George")
+    var ctr : Int = 0
+    while (ctr < names.size) {
+        println("Enter new value for index $ctr")
+        names[ctr] = readln()
+
+        ctr++
+    }
+
+    var ctr2 : Int = 0
+    while (ctr2 < names.size) {
+        println(names[ctr2])
+
+        ctr2++
+    }
+}
