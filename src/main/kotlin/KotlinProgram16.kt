07feccc69253e2a238f3353c4280f396051ffd3b
benedict van velasco <velascobenedict@yahoo.com>
2022-09-07 20:50:55 +0800

    Commit KotlinProgram16

--- src/main/kotlin/KotlinProgram16.kt
+++ src/main/kotlin/KotlinProgram16.kt
@@ -2,17 +2,28 @@ fun main() {
 
     var names = ArrayList<String>()
 
-    names.add("John")
-    names.add("Peter")
+//    names.add("John")
+//    names.add("Peter")
+//
+//    // Add elements
+//    println(names[0])
+//    println(names[1])
+//
+//    // Remove elements
+//    names.removeAt(0) // Peter moves to index 0
+//
+//    // Display names
+//    println(names[0])
 
-    // Add elements
-    println(names[0])
-    println(names[1])
+    names.add("Mary")
+    names.add("Jenny")
+    names.add("Mark")
+    names.add("Paul")
+    names.add("Bryan")
+    names.add("Ruel")
 
-    // Remove elements
-    names.removeAt(0) // Peter moves to index 0
-
-    // Display names
-    println(names[0])
+    for (value in names) {
+        println(" $value")
+    }
 
 }
