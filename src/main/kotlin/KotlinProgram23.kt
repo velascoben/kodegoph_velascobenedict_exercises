38b507920474e890b6ab3bdac327e3f5ac2f640c
benedict van velasco <velascobenedict@yahoo.com>
2022-09-10 14:21:46 +0800

    Commit KotlinProgram23

---
+++ src/main/kotlin/KotlinProgram23.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Enter number:")
+    var result = cube(readln().toInt())
+    println("The cube is $result")
+
+    println("Enter numbers: ")
+    println(add(readln().toInt(), readln().toInt()))
+}
+
+fun cube(number : Int) : Int {
+    var compute = number * number * number
+    //return println("The cube of $number is $compute")
+    return compute
+}
+
+fun add(number1 : Int, number2 : Int) : Int {
+    return number1+number2
+}
