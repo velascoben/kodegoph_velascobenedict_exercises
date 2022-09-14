a2729a84df5ab26ed912283757278bd4c776a983
benedict van velasco <velascobenedict@yahoo.com>
2022-09-14 20:08:26 +0800

    Commit KotlinProgram26

---
+++ src/main/kotlin/KotlinProgram26.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // Lambda
+    var sum : (Int,Int) -> Int = { num1, num2 ->
+        num1 + num2
+    }
+
+    println(sum(2,3))
+}
+
+fun sum(num1 : Int, num2 : Int) : Int {
+    return num1 + num2
+}
