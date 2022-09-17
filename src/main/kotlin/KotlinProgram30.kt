2eec3511ce9fd6d12c7f0addfbc517f9032241cb
benedict van velasco <velascobenedict@yahoo.com>
2022-09-17 13:35:28 +0800

    Commit KotlinProgram30

---
+++ src/main/kotlin/KotlinProgram30.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var num1 : Int = 2
+    var num2 : Int = 3
+
+    var sum : Int = num1 + num2
+    println("The sum of $num1 and $num2 is $sum")
+
+    var ctr : Int = 1
+
+    while (ctr <= 5) {
+        println(ctr)
+        ctr++
+    }
+}
