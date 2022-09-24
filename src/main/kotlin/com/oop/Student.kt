79ebff102d694b739b9417806f9018a4c88f8725
benedict van velasco <velascobenedict@yahoo.com>
2022-09-24 12:16:58 +0800

    Commit Student

--- src/main/kotlin/com/oop/Student.kt
+++ src/main/kotlin/com/oop/Student.kt
@@ -1,4 +1,15 @@
 package com.oop
 
-class Student {
+class Student(_studentID : Int) : Person() {
+
+    var studentID : Int = _studentID
+
+    fun displayStudentID() {
+        println(studentID)
+    }
+
+    override fun displayName() { // Polymorphism
+        super.displayName()
+        println("Displaying function displayName from Student class")
+    }
 }
