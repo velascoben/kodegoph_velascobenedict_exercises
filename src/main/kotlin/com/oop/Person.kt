b305c8dd11f8ce5cc4ae2869d7497a1ecb6b6fba
benedict van velasco <velascobenedict@yahoo.com>
2022-09-24 12:17:27 +0800

    Commit Person

--- src/main/kotlin/com/oop/Person.kt
+++ src/main/kotlin/com/oop/Person.kt
@@ -1,4 +1,44 @@
 package com.oop
 
-class Person {
+open class Person {
+
+    // Abstraction
+    // Encapsulation
+
+    var name : String = "John"
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+    var age : Int = 23
+        get() {
+            return field
+        }
+        set(value) {
+            if(value < 1) {
+                println("Invalid Age!")
+            } else {
+                field = value
+            }
+
+        }
+    init {
+        println("Object Created!")
+        println("Name is $name, age is $age")
+    }
+
+    fun upAge() : Int {
+        age++
+        return age
+    }
+
+    fun changeAge(age : Int) {
+        println(age)
+    }
+
+    open fun displayName() { // Polymorphism
+        println("The name is $name")
+    }
 }
