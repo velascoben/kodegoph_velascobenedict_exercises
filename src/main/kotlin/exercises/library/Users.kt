4288bbc1fa483c4a5d4b794fcec5b151a60924ec
benedict van velasco <velascobenedict@yahoo.com>
2022-10-04 21:01:08 +0800

    Commit Library Users Class

--- src/main/kotlin/exercises/library/Users.kt
+++ src/main/kotlin/exercises/library/Users.kt
@@ -1,4 +1,49 @@
 package exercises.library
 
-class Users {
+open class Users {
+
+    var user : String = ""
+        get() = field
+        set(value){
+            if(value == "") {
+                println("Username cannot be empty!")
+            } else {
+                field = value
+            }
+        }
+
+    var pass : String = ""
+        get() = field
+        set(value){
+            if(value == "") {
+                println("Password cannot be empty!")
+            } else {
+                field = value
+            }
+        }
+
+    var result : Boolean = false
+
+    var usersList = mutableMapOf<String,String>("ben" to "benPass", "aziza" to "AzizaPass", "eugene" to "EugenePass" )
+
+}
+
+class SearchUsers : Users() {
+
+    fun searchUser(user : String, pass : String) : Boolean {
+
+        var i : Int = 0
+        while (i <= usersList.size) {
+            for (userList in usersList.keys) {
+                if(user == userList) {
+                    var getPass : String? = usersList.get(userList)
+                    result = getPass == pass
+                }
+            }
+            i++
+        }
+
+        return result
+
+    }
 }
